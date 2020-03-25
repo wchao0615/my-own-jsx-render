348375d959cd87dd626b157c46fda68b786eb96c
Chao <kevin120795.wang@gmail.com>
2020-03-25 11:58:39 +0800

    Update render.js

--- render.js
+++ render.js
@@ -3,3 +3,25 @@ function f(nodeName, attributes, ...args){
   return { nodeName, attributes, children };
 }
 
+function render(vnode){
+  // 转换成 #text节点
+  if(vnode.split) return document.createTextNode(vnode);
+  
+  // 创建一个带有VDOM元素节点名的DOM 元素
+  let n = document.createElment(vnode.nodeName);
+  
+  // 拷贝属性到新的节点
+  let a = vnode.attributes || {};
+  Object.keys(a).forEach( k => n.setAttributes(k, a[k]);
+  
+  // 渲染并且添加子节点                      
+  (vnode.children || []).forEach(c => n.appendChild(render(c)));
+  return n;
+}
+
+let vdom = <div id="foo">Hello!</div>
+
+let dom = render(vdom);
+
+document.body.appendChild(dom);
+
